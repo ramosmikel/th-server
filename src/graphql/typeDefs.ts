f0252010b9358545c541af84082e83b1aac5ee9f
Mikel Ramos <ramosmikel@icloud.com>
2020-07-25 20:50:54 +0100

    #87 Authentication Rersolvers

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,14 +1,25 @@
 import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
+  type Viewer{
+    id: ID
+    token: String
+    avatar: String
+    hasWallet: Boolean
+    didRequest: Boolean!
+  }
+
+  input LoginInInput {
+    code: String!
+  }
 
   type Query { 
     authUrl: String!
   }
 
   type Mutation { 
-    logIn: String!
-    logOut: String!
+    logIn(input: LoginInInput): Viewer!
+    logOut: Viewer!
   }
 `;
 
