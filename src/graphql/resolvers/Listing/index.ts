bb5a92fce4617dcd2b2b28aa0c9c899dd2a98623
Mikel Ramos <ramosmikel@icloud.com>
2020-07-27 17:31:15 +0100

    #107 Listing GRAPHQL Fields

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -2,6 +2,11 @@ import { IResolvers, IResolverObject } from "apollo-server-express";
 import { Listing, ListingType } from "../../../lib/types";
 
 export const listingResolvers: IResolvers = {
+  Query: {
+    listing: () => {
+      return "Query.listing";
+    }
+  },
   Listing: {
     id: (listing: Listing): string => {
       return listing._id.toString();
